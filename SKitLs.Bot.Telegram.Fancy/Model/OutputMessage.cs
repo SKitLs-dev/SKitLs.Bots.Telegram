4df17864871a0a67dc28ff2b672314dbeb6b4d78
Sergey Kostin <kostinss@me.com>
2023-05-07 20:35:11 +0300

    Advanced Messages Update

--- SKitLs.Bot.Telegram.Fancy/Model/OutputMessage.cs
+++ SKitLs.Bot.Telegram.Fancy/Model/OutputMessage.cs
@@ -1,7 +1,6 @@
 ﻿using SKitLs.Bots.Telegram.Core.Prototypes;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
-using TEnums = Telegram.Bot.Types.Enums;
 
 namespace SKitLs.Bots.Telegram.AdvancedMessages.Model
 {
@@ -20,40 +19,10 @@ namespace SKitLs.Bots.Telegram.AdvancedMessages.Model
             Markup = markup;
             return this;
         }
-        public bool IsParseSafe(ParseMode mode, string part) => mode switch
-        {
-            TEnums.ParseMode.Markdown => IsMarkdownSafe(part),
-            _ => true,
-        };
-        public string MakeParseSafe(ParseMode mode, string part) => mode switch
-        {
-            TEnums.ParseMode.Markdown => MakeMarkdownSafe(part),
-            _ => part,
-        };
 
-        private bool IsMarkdownSafe(string part)
+        public string GetMessageText()
         {
-            int italic = 0;
-            int bold = 0;
-            int stroke = 0;
-            foreach (char c in part)
-            {
-                if (c == '*')
-                    bold++;
-                else if (c == '_')
-                    italic++;
-                else if (c == '~')
-                    stroke++;
-            }
-            return italic % 2 == 0 && bold % 2 == 0 && stroke % 2 == 0;
-        }
-        private string MakeMarkdownSafe(string part)
-        {
-            string res = "";
-            foreach (char c in part)
-                if (c != '*' && c != '~' && c != '_')
-                    res += c;
-            return res;
+            throw new NotImplementedException();
         }
     }
 }
